733029925182060c8071052ccbc41a8478b65a33
7S-Codere <dylan.e33@hotmail.fr>
2025-11-08 09:17:31 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
+import logo from '../assets/logo.png';
+import '../css/pages/components/Header.css';
+import { Search } from 'lucide-react';
+
 export default function Header() {
+
     return (
         <header>
-            <h1>Bienvenue sur Trouve Ton Artisan</h1>
+            <nav>
+                <img src={logo} alt="Logo de trouve ton artisan" />
+                <div>
+                    <section id="search-bar">
+                        <input type="text" />
+                        <button> <Search size={16}/></button>
+                    </section>
+                    
+                    <ul>
+                        <li><a href="#">Batiment</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Fabrication</a></li>
+                        <li><a href="#">Alimentation</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     );
 }
